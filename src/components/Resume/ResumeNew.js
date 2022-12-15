32e6ed887e1c59d68d277423f8dd29ff04b3323b
mobina <mobina.km@gmail.com>
2022-12-15 11:13:56 +0330

    add resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Mobina_Kashaniyan_s_Resume_updated.pdf";
+  "https://iammobina.github.io/Mobina_Kashaniyan_s_Resume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
